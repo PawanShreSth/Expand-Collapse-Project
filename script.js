412ca34716f321980b72defad4dc4bee47056f1a
Pawan Shrestha <pawanshresthait@gmail.com>
2021-06-22 18:51:05 +0545

    Added functionality to button

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const body = document.body;
 const cardBody = document.querySelector('.card-body');
+
+body.addEventListener('click', e => {
+  if (!e.target.matches('.expand-button')) return;
+
+  const clickedButton = e.target;
+
+  const card = clickedButton.closest('.card');
+
+  const cardBody = card.querySelector('.card-body');
+
+  cardBody.classList.toggle('show');
+
+  if (!cardBody.classList.contains('show')) {
+    clickedButton.innerText = 'Expand';
+    return;
+  }
+
+  clickedButton.innerText = 'Collapse';
+});
